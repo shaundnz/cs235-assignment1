be1d06101d4c07c870baacacfddbd224d2ce5e51
Shaun Price <shaundnz@gmail.com>
2020-08-15 14:57:33 +1200

    Initial commit.

---
+++ domainmodel/director.py
@@ -0,0 +1,38 @@
+
+class Director:
+
+    def __init__(self, director_full_name: str):
+        if director_full_name == "" or type(director_full_name) is not str:
+            self.__director_full_name = None
+        else:
+            self.__director_full_name = director_full_name.strip()
+
+    @property
+    def director_full_name(self) -> str:
+        return self.__director_full_name
+
+    def __repr__(self):
+        return f"<Director {self.__director_full_name}>"
+
+    def __eq__(self, other):
+        # TODO
+        pass
+
+    def __lt__(self, other):
+        # TODO
+        pass
+
+    def __hash__(self):
+        # TODO
+        pass
+
+
+class TestDirectorMethods:
+
+    def test_init(self):
+        director1 = Director("Taika Waititi")
+        assert repr(director1) == "<Director Taika Waititi>"
+        director2 = Director("")
+        assert director2.director_full_name is None
+        director3 = Director(42)
+        assert director3.director_full_name is None
